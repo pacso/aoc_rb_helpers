4a8164c1f52bacd1fd5d984a331498bb53e2ea90
Jon Pascoe <jon.pascoe@me.com>
2024-12-02 13:19:44 +0000

    Update version in spec

--- spec/lib/aoc_rb_helpers_spec.rb
+++ spec/lib/aoc_rb_helpers_spec.rb
@@ -4,6 +4,6 @@ require 'spec_helper'
 
 RSpec.describe AocRbHelpers do
   it "has the expected version number" do
-    expect(AocRbHelpers::VERSION).to eq "0.0.2"
+    expect(AocRbHelpers::VERSION).to eq "0.0.3"
   end
 end
