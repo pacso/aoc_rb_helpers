bf289db95f76b0c3c9681345690879370f6d7c6f
Jon Pascoe <jon.pascoe@me.com>
2024-12-02 07:25:48 +0000

    Add version spec

---
+++ spec/lib/aoc_rb_helpers_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe AocRbHelpers do
+  it "has the expected version number" do
+    expect(AocRbHelpers::VERSION).to eq "0.0.2"
+  end
+end
