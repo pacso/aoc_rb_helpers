03b3f1001f137739d0ef05a6b59fbeb10fd01084
Jon Pascoe <jon.pascoe@me.com>
2024-12-05 06:53:39 +0000

    Add to_grid and sections methods along with documentation updates

--- lib/aoc_rb_helpers/aoc_input.rb
+++ lib/aoc_rb_helpers/aoc_input.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 # Provides input manipulation helper methods.
-# Methods are chainable, and directly modify the parsed view of the input data within the @data instance variable.
+# Methods are chainable, and directly modify the parsed view of the input data within the +@data+ instance variable.
 #
-# Once manipulated as required, the input is accessable from #data
+# Once manipulated as required, the input is accessable using the {#data} method.
 class AocInput
   # Returns a new AocInput initialized with the given puzzle input.
   #
@@ -24,8 +24,8 @@ class AocInput
 
   # Splits the input string into an array of lines.
   #
-  # This method processes `@data` by splitting the input string into multiple lines,
-  # removing trailing newline characters. It modifies `@data` directly and returns `self`
+  # This method processes +@data+ by splitting the input string into multiple lines,
+  # removing trailing newline characters. It modifies +@data+ directly and returns +self+
   # to enable method chaining.
   #
   # @return [AocInput] self
@@ -38,11 +38,11 @@ class AocInput
 
   # Splits each string in the data array into an array of numbers.
   #
-  # This method processes `@data` by splitting each string in the array using the specified delimiter,
-  # then converting each resulting element to an integer. It modifies `@data` directly and enables
-  # chaining by returning `self`.
+  # This method processes +@data+ by splitting each string in the array using the specified delimiter,
+  # then converting each resulting element to an integer. It modifies +@data+ directly and enables
+  # chaining by returning +self+.
   #
-  # @param delimiter [String, nil] the delimiter to be passed to `String#split`
+  # @param delimiter [String, nil] the delimiter to be passed to +String#split+
   # @raise [RuntimeError] if {#multiple_lines} has not been called
   # @return [AocInput] self
   def columns_of_numbers(delimiter = nil)
@@ -57,7 +57,7 @@ class AocInput
   # Transposes the data array.
   #
   # This method can only be called after {columns_of_numbers}.
-  # It directly modifies `@data` by transposing it and returns `self` to allow method chaining.
+  # It directly modifies +@data+ by transposing it and returns +self+ to allow method chaining.
   #
   # @raise [RuntimeError] if {columns_of_numbers} has not been called.
   # @return [AocInput] self
@@ -67,10 +67,10 @@ class AocInput
     self
   end
 
-  # Sorts each array within the `@data` array.
+  # Sorts each array within the +@data+ array.
   #
-  # This method processes `@data` by sorting each nested array in ascending order.
-  # It directly modifies `@data` and returns `self` to enable method chaining.
+  # This method processes +@data+ by sorting each nested array in ascending order.
+  # It directly modifies +@data+ and returns +self+ to enable method chaining.
   #
   # @raise [RuntimeError] if {#columns_of_numbers} has not been called
   # @return [AocInput] self
@@ -78,6 +78,19 @@ class AocInput
     can_call?(:sort_arrays, "call .columns_of_numbers first")
     @data = data.map { |ary| ary.sort }
     self
+  end
+
+  # Returns a new +Grid+ object from the parsed input
+  # @return [Grid] the new grid
+  def to_grid
+    Grid.from_input(@raw_input)
+  end
+
+  # Returns a new +AocInput+ for each section of the raw input, split by the given delimiter.
+  # @param delimiter [String] the string used to split sections
+  # @return [Array<AocInput>] an array of new AocInput instances initialised with each section of the raw input from +self+
+  def sections(delimiter = "\n\n")
+    @sections = @raw_input.split(delimiter).map { |section_input| AocInput.new(section_input) }
   end
 
   private
